9248e5e237682fff97c97896eaed5017d1f3ad71
caiyunl.lei <caiyunl.lei@qunar.com>
2018-08-19 18:11:50 +0800

    新增路由页面设置title

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,7 @@ Vue.use(Router);
 const proxylist = () => import('@/view/proxy-list')
 const proxydetail = () => import('@/view/proxy-detail')
 
-
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -16,11 +15,26 @@ export default new Router({
       path: '/proxylist',
       name: 'proxylist',
       component: proxylist,
+      meta: {
+        title: '代理列表'
+      }
     },
     {
       path: '/proxydetail',
       name: 'proxydetail',
       component: proxydetail,
+      meta: {
+        title: '代理详情'
+      }
     },
   ],
 });
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+export default router;
